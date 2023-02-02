b95ef3c84341505516ea2bdbf628c9b2f93e1b5f
Diletta Pelloni <dilpel@live.it>
2023-02-02 18:00:11 +0100

    pronto stile header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,6 +3,7 @@
         name: 'AppHeader',
         data() {
             return {
+                active: 0,
                 navbar: [
                     {
                         title: 'characters',
@@ -59,8 +60,14 @@
             </div><!-- CHIUSURA LOGO-BOX  -->
             <div class="navbar">
                 <ul>
-                    <li v-for="navitem in navbar">
-                        <a :href="navitem.link"> {{ navitem.title }} </a>
+                    <li v-for="(navitem, i) in navbar">
+                        <a
+                        :href="navitem.link"
+                        :class="{
+                            active: active == i,
+                        }">
+                            {{ navitem.title }}
+                        </a>
                     </li>
                 </ul>
             </div><!-- CHIUSUR NAVBAR  -->
@@ -70,28 +77,46 @@
 </template>
 
 <style lang="scss" scoped>
+    @import "../style/partials/colors.scss";
 
     header {
+        height: 150px;
+        background-color: white;
         .container {
+            height: 100%;
             display: flex;
             justify-content: space-between;
-
+            align-items: center;
+            .logo-box {
+                padding: 1rem 0;
+                height: 100%;
+                img {
+                    height: 100%;
+                }
+            }
             ul {
                 list-style: none;
                 display: flex;
+                a {
+                    display: inline-block;
+                    padding: 0 1rem;
+                    height: 150px;
+                    line-height: 150px;
+                    text-transform: uppercase;
+                    font-weight: bold;
+                    color: $nq-black;
+                    text-decoration: none;
 
-                li {
-
-                    a {
-                        display: inline-block;
-                        padding: 0.5rem 1rem;
-                        text-decoration: none;
-                        background-color: salmon;
-                        border: 1px solid black;
+                    &.active {
+                        color: $dc-blue;
+                        box-shadow: inset 0px -10px 0px 0px $dc-blue;
                     }
-                }
-            }
-        }
-    }
+                    &:hover {
+                        color: $dc-blue;
+                    }
+                }//a
+            }//ul
+        }//container
+    }//header
 
 </style>
